4626e19dbd84e4dddf330fd3c4c2a0c2c866abdd
Andrzej Kalinowski <jekrekk75@gmail.com>
2023-03-15 22:58:47 +0100

    testing tachometer, propably dosen't work yet

---
+++ pico_tacho_test/pico_tacho_test.ino
@@ -0,0 +1,29 @@
+const byte ledPin = 25;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
+float timeee = 0;
+float prev_timeee = 0;
+float counter = 0;
+float delta_t_s = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), tacho, RISING);
+}
+
+void loop(){
+  detachInterrupt(digitalPinToInterrupt(interruptPin));
+  timeee = millis();
+  delta_t_s = (timeee - prev_timeee)/1000;
+  Serial.println((counter/delta_t_s)*60);
+  counter = 0;
+  prev_timeee = timeee;
+  attachInterrupt(digitalPinToInterrupt(interruptPin), tacho, RISING);
+  delay(1000);
+}
+
+void tacho() {
+  counter += 0.5;
+}
