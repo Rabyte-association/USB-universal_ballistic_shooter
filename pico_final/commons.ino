5083ae4f02b00d7b8f6f40198251746e8f6a29f0
Minusplus <minusplus814@gmail.com>
2023-04-15 12:42:25 +0200

    pico code /w servos

--- pico_final/commons.ino
+++ pico_final/commons.ino
@@ -5,11 +5,11 @@ void print_debug(){
   Serial.print(", ");
   Serial.println(actual_speed2);
 
-  Serial.print(setpiont_speed0);
+  Serial.print(setpoint_speed0);
   Serial.print(", ");
-  Serial.print(setpiont_speed1);
+  Serial.print(setpoint_speed1);
   Serial.print(", ");
-  Serial.println(setpiont_speed2);
+  Serial.println(setpoint_speed2);
 }
 
 void motor0_writeRPM(double rpm){
@@ -17,7 +17,11 @@ void motor0_writeRPM(double rpm){
   if(out_deg > 180) out_deg = 180;
   motor0.write(out_deg);
 }
-
+void fan_writeRPM(int percent){
+  int out_deg = map(percent, 0, 100, 0, 180); 
+  if(out_deg > 180) out_deg = 180;
+  fan.write(out_deg);
+}
 void motor1_writeRPM(double rpm){
   int out_deg = map(rpm, 0, MAX_RPM, 0, 180); 
   if(out_deg > 180) out_deg = 180;
@@ -28,4 +32,11 @@ void motor2_writeRPM(double rpm){
   int out_deg = map(rpm, 0, MAX_RPM, 0, 180); 
   if(out_deg > 180) out_deg = 180;
   motor2.write(out_deg);
+}
+
+void open_top(){
+  
+}
+void open_bot(){
+  
 }
--- pico_final/pico_final.ino
+++ pico_final/pico_final.ino
@@ -17,6 +17,14 @@ const byte motor2_interruptPin = 20;
 const byte motor0_pin = 17;
 const byte motor1_pin = 19;
 const byte motor2_pin = 21;
+const byte servoTop_pin = 2;
+const byte servoBot_pin = 3;
+const byte fan_pin = 3;
+
+const byte closedtop = 125;
+const byte openedtop=100;
+const byte closedbottom=160;
+const byte openedbottom=100;
 
 float current_time = 0;
 float prev_time = 0;
@@ -24,14 +32,20 @@ float delta_t_s = 0;
 float counter0 = 0;
 float counter1 = 0;
 float counter2 = 0;
+int fan_speed=0;
+int singleShot_flag=2;
+float openTime =0;
+float closedTime =0;
+
+
 
 double actual_speed0;
 double actual_speed1;
 double actual_speed2;
 
-double setpiont_speed0 = 0;
-double setpiont_speed1 = 0;
-double setpiont_speed2 = 0;
+double setpoint_speed0 = 0;
+double setpoint_speed1 = 0;
+double setpoint_speed2 = 0;
 
 double written_speed0 = 0;
 double written_speed1 = 0;
@@ -40,11 +54,14 @@ double written_speed2 = 0;
 Servo motor0;
 Servo motor1;
 Servo motor2;
+Servo fan;
+Servo feedtop;
+Servo feedbot;
 
 double Kp=2, Ki=5, Kd=1;
-PID motor0_PID(&actual_speed0, &written_speed0, &setpiont_speed0, Kp, Ki, Kd, DIRECT);
-PID motor1_PID(&actual_speed1, &written_speed1, &setpiont_speed1, Kp, Ki, Kd, DIRECT);
-PID motor2_PID(&actual_speed2, &written_speed2, &setpiont_speed2, Kp, Ki, Kd, DIRECT);
+PID motor0_PID(&actual_speed0, &written_speed0, &setpoint_speed0, Kp, Ki, Kd, DIRECT);
+PID motor1_PID(&actual_speed1, &written_speed1, &setpoint_speed1, Kp, Ki, Kd, DIRECT);
+PID motor2_PID(&actual_speed2, &written_speed2, &setpoint_speed2, Kp, Ki, Kd, DIRECT);
 bool use_PID = true;
 
 void tacho0() {
@@ -72,6 +89,10 @@ void setup() {
   motor0.attach(motor0_pin);
   motor1.attach(motor1_pin);
   motor2.attach(motor2_pin);
+  fan.attach(fan_pin);
+  feedtop.attach(servoTop_pin);
+  feedbot.attach(servoBot_pin);
+
 
   motor0_PID.SetMode(AUTOMATIC);
   motor1_PID.SetMode(AUTOMATIC);
@@ -82,30 +103,32 @@ void loop() {
   if (Serial.available() > 1) {
     byte data = Serial.read();
     if (data == 's') {
-      written_speed0 = 0;
-      written_speed1 = 0;
-      written_speed2 = 0;
-    }
+      setpoint_speed0 = 0;
+      setpoint_speed1 = 0;
+      setpoint_speed2 = 0;    }
     if (data == 'f') {
-      setpiont_speed0 = 180;
-      setpiont_speed1 = 180;
-      setpiont_speed2 = 180;
+      setpoint_speed0 = 180;
+      setpoint_speed1 = 180;
+      setpoint_speed2 = 180;
     }
     if (data == 'u') {
       int tmp = Serial.parseInt();
-      setpiont_speed0 = tmp;
-      setpiont_speed1 = tmp;
-      setpiont_speed2 = tmp;
+      setpoint_speed0 = tmp;
+      setpoint_speed1 = tmp;
+      setpoint_speed2 = tmp;
       tmp = 0;
     }
     if (data == 'a') {
-      setpiont_speed0 = Serial.parseInt();
+      setpoint_speed0 = Serial.parseInt();
     }
     if (data == 'b') {
-      setpiont_speed1 = Serial.parseInt();
+      setpoint_speed1 = Serial.parseInt();
     }
     if (data == 'c') {
-      setpiont_speed0 = Serial.parseInt();
+      setpoint_speed0 = Serial.parseInt();
+    }
+    if (data == 'w') {
+      fan_speed = Serial.parseInt();
     }
     if (data == 'p') {
       use_PID = true;
@@ -114,6 +137,21 @@ void loop() {
     if (data == 'q') {
       use_PID = false;
       Serial.println("PID disabled");
+    }
+    if (data == 't') {
+      feedtop.write(openedtop);
+      delay(200);
+      feedtop.write(closedtop);
+    }
+    if(data=='y'){
+      feedbot.write(openedbottom);
+      delay(250);
+      feedbot.write(closedbottom);
+    }
+    if(data=='l'){
+      singleShot_flag=2;feedbot.write(openedbottom);
+      openTime = millis();
+      feedtop.write(openedtop);
     }
   }
   current_time = millis();
@@ -127,20 +165,33 @@ void loop() {
   prev_time = current_time;
   
   print_debug();
-  
+  if(singleShot_flag>0 && millis()>openTime+225){
+    singleShot_flag--;
+    if(singleShot_flag==1){
+    feedtop.write(closedtop);
+    openTime=millis();
+    feedbot.write(openedbottom);
+    }
+    else{
+    feedbot.write(closedbottom);
+    }
+    
+  }
   if(use_PID){
     motor0_PID.Compute();
     motor1_PID.Compute();
     motor2_PID.Compute();
   }
   else{
-    written_speed0 = setpiont_speed0;
-    written_speed1 = setpiont_speed1;
-    written_speed2 = setpiont_speed2;
+    written_speed0 = setpoint_speed0;
+    written_speed1 = setpoint_speed1;
+    written_speed2 = setpoint_speed2;
   }
   motor0_writeRPM(written_speed0);
   motor1_writeRPM(written_speed1);
   motor2_writeRPM(written_speed2);
+  fan_writeRPM(fan_speed);
+
   
   delay(1);
 }
