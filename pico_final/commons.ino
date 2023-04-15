71cec4607762bb7ec212d9368e41d4b4b792a747
Andrzej Kalinowski <jekrekk75@gmail.com>
2023-04-15 21:40:53 +0200

    pico code without pid, speed measurement deg instead of rpm tried fixing the rpm calcoulation using multicore rpm dosen't work well, not using it

--- pico_final/commons.ino
+++ pico_final/commons.ino
@@ -1,42 +1,44 @@
-void print_debug(){
+void print_debug() {
   Serial.print(actual_speed0);
   Serial.print(", ");
   Serial.print(actual_speed1);
   Serial.print(", ");
   Serial.println(actual_speed2);
 
-  Serial.print(setpoint_speed0);
-  Serial.print(", ");
-  Serial.print(setpoint_speed1);
-  Serial.print(", ");
-  Serial.println(setpoint_speed2);
+  //  Serial.print(setpoint_speed0);
+  //  Serial.print(", ");
+  //  Serial.print(setpoint_speed1);
+  //  Serial.print(", ");
+  //  Serial.println(setpoint_speed2);
 }
-
-void motor0_writeRPM(double rpm){
-  int out_deg = map(rpm, 0, MAX_RPM, 0, 180); 
-  if(out_deg > 180) out_deg = 180;
-  motor0.write(out_deg);
-}
-void fan_writeRPM(int percent){
-  int out_deg = map(percent, 0, 100, 0, 180); 
-  if(out_deg > 180) out_deg = 180;
+void fan_writeRPM(int percent) {
+  int out_deg = map(percent, 0, 100, 0, 180);
+  if (out_deg > 180) out_deg = 180;
   fan.write(out_deg);
 }
-void motor1_writeRPM(double rpm){
-  int out_deg = map(rpm, 0, MAX_RPM, 0, 180); 
-  if(out_deg > 180) out_deg = 180;
+
+void motor0_writeRPM(double rpm) {
+  //  int out_deg = map(rpm, 0, MAX_RPM, 0, 180);
+  //  if(out_deg > 180) out_deg = 180;
+  int out_deg = rpm;
+  motor0.write(out_deg);
+}
+void motor1_writeRPM(double rpm) {
+  //  int out_deg = map(rpm, 0, MAX_RPM, 0, 180);
+  //  if(out_deg > 180) out_deg = 180;
+  int out_deg = rpm;
   motor1.write(out_deg);
 }
-
-void motor2_writeRPM(double rpm){
-  int out_deg = map(rpm, 0, MAX_RPM, 0, 180); 
-  if(out_deg > 180) out_deg = 180;
+void motor2_writeRPM(double rpm) {
+  //  int out_deg = map(rpm, 0, MAX_RPM, 0, 180);
+  //  if(out_deg > 180) out_deg = 180;
+  int out_deg = rpm;
   motor2.write(out_deg);
 }
 
-void open_top(){
-  
+void open_top() {
+
 }
-void open_bot(){
-  
+void open_bot() {
+
 }
--- pico_final/pico_final.ino
+++ pico_final/pico_final.ino
@@ -6,7 +6,7 @@
 #include <Servo.h>
 #include <PID_v1.h>
 
-#define MAX_RPM 14400
+#define MAX_RPM 13500
 
 const byte ledPin = 25;
 
@@ -19,23 +19,24 @@ const byte motor1_pin = 19;
 const byte motor2_pin = 21;
 const byte servoTop_pin = 2;
 const byte servoBot_pin = 3;
-const byte fan_pin = 3;
+const byte fan_pin = 1;
 
 const byte closedtop = 125;
-const byte openedtop=100;
-const byte closedbottom=160;
-const byte openedbottom=100;
+const byte openedtop = 100;
+const byte closedbottom = 160;
+const byte openedbottom = 100;
 
-float current_time = 0;
-float prev_time = 0;
-float delta_t_s = 0;
-float counter0 = 0;
-float counter1 = 0;
-float counter2 = 0;
-int fan_speed=0;
-int singleShot_flag=2;
-float openTime =0;
-float closedTime =0;
+uint32_t current_time = 0;
+uint32_t prev_time = 0;
+uint32_t delta_t_s = 0;
+uint32_t counter0 = 0;
+uint32_t counter1 = 0;
+uint32_t counter2 = 0;
+
+int fan_speed = 0;
+int singleShot_flag = 2;
+uint32_t openTime = 0;
+uint32_t closedTime = 0;
 
 
 
@@ -58,11 +59,11 @@ Servo fan;
 Servo feedtop;
 Servo feedbot;
 
-double Kp=2, Ki=5, Kd=1;
+double Kp = 2, Ki = 5, Kd = 1;
 PID motor0_PID(&actual_speed0, &written_speed0, &setpoint_speed0, Kp, Ki, Kd, DIRECT);
 PID motor1_PID(&actual_speed1, &written_speed1, &setpoint_speed1, Kp, Ki, Kd, DIRECT);
 PID motor2_PID(&actual_speed2, &written_speed2, &setpoint_speed2, Kp, Ki, Kd, DIRECT);
-bool use_PID = true;
+bool use_PID = false;
 
 void tacho0() {
   counter0 += 1;
@@ -78,7 +79,7 @@ void setup() {
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
-  
+
   pinMode(motor0_interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(motor0_interruptPin), tacho0, RISING);
   pinMode(motor1_interruptPin, INPUT_PULLUP);
@@ -93,19 +94,67 @@ void setup() {
   feedtop.attach(servoTop_pin);
   feedbot.attach(servoBot_pin);
 
-
   motor0_PID.SetMode(AUTOMATIC);
   motor1_PID.SetMode(AUTOMATIC);
   motor2_PID.SetMode(AUTOMATIC);
 }
 
+void setup1() {
+
+}
+
 void loop() {
+  current_time = millis();
+  delta_t_s = (current_time - prev_time) / 1000;
+  actual_speed0 = (counter0 / delta_t_s) * 60;
+  actual_speed1 = (counter1 / delta_t_s) * 60;
+  actual_speed2 = (counter2 / delta_t_s) * 60;
+  //  Serial.println(counter0);
+  counter0 = 0;
+  counter1 = 0;
+  counter2 = 0;
+  prev_time = current_time;
+
+  print_debug();
+
+  if (singleShot_flag > 0 && millis() > openTime + 180) {
+    singleShot_flag--;
+    if (singleShot_flag == 1) {
+      feedtop.write(closedtop);
+      openTime = millis();
+      feedbot.write(openedbottom);
+    }
+    else {
+      feedbot.write(closedbottom);
+    }
+
+  }
+  if (use_PID) {
+    motor0_PID.Compute();
+    motor1_PID.Compute();
+    motor2_PID.Compute();
+  }
+  else {
+    written_speed0 = setpoint_speed0;
+    written_speed1 = setpoint_speed1;
+    written_speed2 = setpoint_speed2;
+  }
+  motor0_writeRPM(written_speed0);
+  motor1_writeRPM(written_speed1);
+  motor2_writeRPM(written_speed2);
+  fan_writeRPM(fan_speed);
+
+  delay(5);
+}
+
+void loop1() {
   if (Serial.available() > 1) {
     byte data = Serial.read();
     if (data == 's') {
       setpoint_speed0 = 0;
       setpoint_speed1 = 0;
-      setpoint_speed2 = 0;    }
+      setpoint_speed2 = 0;
+    }
     if (data == 'f') {
       setpoint_speed0 = 180;
       setpoint_speed1 = 180;
@@ -143,55 +192,15 @@ void loop() {
       delay(200);
       feedtop.write(closedtop);
     }
-    if(data=='y'){
+    if (data == 'y') {
       feedbot.write(openedbottom);
       delay(250);
       feedbot.write(closedbottom);
     }
-    if(data=='l'){
-      singleShot_flag=2;feedbot.write(openedbottom);
+    if (data == 'l') {
+      singleShot_flag = 2; feedbot.write(openedbottom);
       openTime = millis();
       feedtop.write(openedtop);
     }
   }
-  current_time = millis();
-  delta_t_s = (current_time - prev_time) / 1000;
-  actual_speed0 = (counter0 / delta_t_s) * 60;
-  actual_speed1 = (counter1 / delta_t_s) * 60;
-  actual_speed2 = (counter2 / delta_t_s) * 60;
-  counter0 = 0;
-  counter1 = 0;
-  counter2 = 0;
-  prev_time = current_time;
-  
-  print_debug();
-  if(singleShot_flag>0 && millis()>openTime+225){
-    singleShot_flag--;
-    if(singleShot_flag==1){
-    feedtop.write(closedtop);
-    openTime=millis();
-    feedbot.write(openedbottom);
-    }
-    else{
-    feedbot.write(closedbottom);
-    }
-    
-  }
-  if(use_PID){
-    motor0_PID.Compute();
-    motor1_PID.Compute();
-    motor2_PID.Compute();
-  }
-  else{
-    written_speed0 = setpoint_speed0;
-    written_speed1 = setpoint_speed1;
-    written_speed2 = setpoint_speed2;
-  }
-  motor0_writeRPM(written_speed0);
-  motor1_writeRPM(written_speed1);
-  motor2_writeRPM(written_speed2);
-  fan_writeRPM(fan_speed);
-
-  
-  delay(1);
 }
