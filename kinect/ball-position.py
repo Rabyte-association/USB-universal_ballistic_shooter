5fc0efd323157d019707789cb2fb8065838a0f8c
pawel <janina.zdruj.2137@wp.pl>
2023-04-15 12:01:49 +0200

    ball tracking

--- kinect/ball-position.py
+++ kinect/ball-position.py
@@ -1,12 +1,22 @@
 import cv2
 import imutils
 import numpy as np
-cap = cv2.VideoCapture(0)
+import random
+
+cap = cv2.VideoCapture(2)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+overlay = np.zeros([720,1280,3], dtype=np.uint8)
+
+old_point = None
 while True:
     _, frame = cap.read()
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # orange
+    #low = np.array([0, 169, 164])
+    #high = np.array([35, 255, 255])
     low = np.array([15, 120, 120])
     high = np.array([35, 255, 255])
     mask = cv2.inRange(hsv_frame, low, high)
@@ -15,6 +25,7 @@ while True:
     center = None
 
     # center
+    rad = 1
     if len(cnts) > 0:
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
@@ -28,11 +39,26 @@ while True:
 				(0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
             cv2.circle(mask, center, 5, (0, 0, 255), -1)
+        rad = radius
 
 
     cv2.imshow("Frame", frame) # video + circle
-    print(center)
-    cv2.imshow("Result", mask) # ball shape
+    #print(center)
+    if center == None:
+        center = old_point
+    if old_point == None:
+        old_point = center
+    if(int(rad) > 0 and int(rad) < 50):
+        overlay = cv2.line(overlay, old_point, center, (random.randint(0,255),random.randint(0,255),random.randint(0,255)), int(2))
+    combined = cv2.addWeighted(frame,1,overlay,1,1)
+    if center != None:
+        old_point = center
+    combined = cv2.flip(combined, 1)
+    cv2.imshow("Result", combined) # ball shape
+    print(rad)
     key = cv2.waitKey(1)
     if key == 27:
         break
+    if key == 32:
+        overlay = np.zeros([720,1280,3], dtype=np.uint8)
+
